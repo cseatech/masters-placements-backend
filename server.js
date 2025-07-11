99beef1166c19cd51ccdfc443cbf729809978ec5
Selva Rathinam  M <selva152004@gmail.com>
2025-07-11 21:36:54 +0530

    Changes2

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.get("/", (req, res) => {
 });
 app.use("/api/users", require("./routes/userRoutes.js"));
 app.use("/api/experiences", require("./routes/experienceRoutes.js"));
-app.listen(PORT, () => {
-    console.log(`Server is listening on ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is listening on ${PORT}`);
+// });
+
+module.exports = app;
